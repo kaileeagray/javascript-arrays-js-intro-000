e6acc6f7c22b1b0bbd6e31e93f71c8ef253e5518
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 18:08:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.unshift(element);
+  array.push(element);
   return array;
 }
