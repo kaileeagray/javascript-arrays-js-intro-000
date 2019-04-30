c5c002fe797559f4646a50c35a0eb4783044aa54
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 18:12:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
